a722570c25ada59d65fbe8fa34801c42e60d9919
soneda-yuya <soneda.yuya@gmail.com>
2025-02-21 16:23:49 +0900

    fix: golint

--- server/internal/app/public.go
+++ server/internal/app/public.go
@@ -194,7 +194,8 @@ func PublishedIndexMiddleware(pattern string, useParam, errorIfNotFound bool) ec
 }
 
 func PublishedAuthMiddleware() echo.MiddlewareFunc {
-	key := struct{}{}
+	type contextKey struct{}
+	var key = contextKey{}
 	return middleware.BasicAuthWithConfig(middleware.BasicAuthConfig{
 		Validator: func(user string, password string, c echo.Context) (bool, error) {
 			md, ok := c.Request().Context().Value(key).(interfaces.ProjectPublishedMetadata)

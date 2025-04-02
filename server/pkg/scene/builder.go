60dd5796143bb3e7b0b325fbecb823b925b665f2
akiyatomohiro <thairwars@gmail.com>
2025-04-02 22:18:39 +0700

    make-rootlayer-of-scene-optional

--- server/pkg/scene/builder.go
+++ server/pkg/scene/builder.go
@@ -22,10 +22,6 @@ func (b *Builder) Build() (*Scene, error) {
 		log.Printf("Build failed: scene.workspace is nil (scene id: %s)", b.scene.id.String())
 		return nil, ErrInvalidID
 	}
-	if b.scene.rootLayer.IsNil() {
-		log.Printf("Build failed: scene.rootLayer is nil (scene id: %s)", b.scene.id.String())
-		return nil, ErrInvalidID
-	}
 	if b.scene.widgets == nil {
 		b.scene.widgets = NewWidgets(nil, nil)
 	}

70900056a7c6c9b9fa7d8470d0ec9072f34238d4
tantaka tomokazu <t.tantaka@eukarya.io>
2025-06-02 17:25:29 +0900

    fix(server): fix dataattributes migration [VIZ-1693] (#82)

--- server/internal/infrastructure/mongo/migration/250602123621_add_default_data_attribution_widget.go
+++ server/internal/infrastructure/mongo/migration/250602123621_add_default_data_attribution_widget.go
@@ -31,7 +31,6 @@ func AddDefaultDataAttributionWidget(ctx context.Context, c DBClient) error {
 			log.Infofc(ctx, "migration: AddDefaultDataAttributionWidget: matched scenes: %d", len(rows))
 
 			for _, row := range rows {
-
 				var scene bson.M
 				if err := bson.Unmarshal(row, &scene); err != nil {
 					return err
@@ -47,6 +46,8 @@ func AddDefaultDataAttributionWidget(ctx context.Context, c DBClient) error {
 				}
 
 				propertyID := id.NewPropertyID().String()
+				widgetID := id.NewWidgetID().String()
+
 				newProperty := bson.M{
 					"id":           propertyID,
 					"scene":        sceneID,
@@ -54,12 +55,10 @@ func AddDefaultDataAttributionWidget(ctx context.Context, c DBClient) error {
 					"schemaname":   "dataattribution",
 					"items":        bson.A{},
 				}
-
 				if _, err := propertyCol.Client().InsertOne(ctx, newProperty); err != nil {
 					return err
 				}
 
-				widgetID := id.NewWidgetID().String()
 				newWidget := bson.M{
 					"id":        widgetID,
 					"plugin":    "reearth",
@@ -67,6 +66,37 @@ func AddDefaultDataAttributionWidget(ctx context.Context, c DBClient) error {
 					"property":  propertyID,
 					"enabled":   true,
 					"extended":  false,
+				}
+
+				needsAlignInit := false
+				if scene["alignsystem"] == nil {
+					needsAlignInit = true
+				} else if outer, ok := scene["alignsystem"].(bson.M)["outer"]; !ok || outer == nil {
+					needsAlignInit = true
+				} else if left, ok := outer.(bson.M)["left"]; !ok || left == nil {
+					needsAlignInit = true
+				} else if bottom, ok := left.(bson.M)["bottom"]; !ok || bottom == nil {
+					needsAlignInit = true
+				} else if _, ok := bottom.(bson.M)["widgetids"]; !ok {
+					needsAlignInit = true
+				}
+
+				if needsAlignInit {
+					_, err := sceneCol.Client().UpdateByID(ctx, objectID, bson.M{
+						"$set": bson.M{
+							"alignsystem.outer.left.bottom": bson.M{
+								"widgetids":  bson.A{},
+								"align":      "start",
+								"padding":    nil,
+								"gap":        nil,
+								"centered":   false,
+								"background": nil,
+							},
+						},
+					})
+					if err != nil {
+						return err
+					}
 				}
 
 				update := bson.M{
@@ -78,8 +108,8 @@ func AddDefaultDataAttributionWidget(ctx context.Context, c DBClient) error {
 				if _, err := sceneCol.Client().UpdateByID(ctx, objectID, update); err != nil {
 					return err
 				}
-
 			}
+
 			return nil
 		},
 	})
